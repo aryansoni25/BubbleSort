8269db35da454c18086d98b1a43958652dc06f35
aryansoni25 <ashoksoni9425@gmail.com>
2025-12-12 20:54:37 +0530

    1st commit

--- src/main/java/org/studyeasy/BubbleSort.java
+++ src/main/java/org/studyeasy/BubbleSort.java
@@ -14,6 +14,7 @@ public class BubbleSort {
         }
     }
 
+
     void printArray(int arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
--- src/main/java/org/studyeasy/Main.java
+++ src/main/java/org/studyeasy/Main.java
@@ -1,5 +1,4 @@
 package org.studyeasy;
-
 public class Main {
     public static void main(String[] args) {
         BubbleSort bubbleSort=new BubbleSort();
