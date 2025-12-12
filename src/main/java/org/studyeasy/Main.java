4becdefd2781ec6d61fd78e836ebfdf3b0c5ec45
aryansoni25 <ashoksoni9425@gmail.com>
2025-12-12 20:53:38 +0530

    Initial commit

---
+++ src/main/java/org/studyeasy/Main.java
@@ -0,0 +1,10 @@
+package org.studyeasy;
+
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort bubbleSort=new BubbleSort();
+        int arr[]={1,50,60,2,0,91,34,21,4};
+        bubbleSort.bubbleSort(arr);
+        bubbleSort.printArray(arr);
+    }
+}
